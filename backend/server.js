4b20eb3b3acf71e5c8235305142fa061d25a5eab
Furkan <turkogluvelifurkan@gmail.com>
2023-01-09 23:11:28 +0300

    attempt to fix map issue

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,14 @@ app.use(express.urlencoded({ extended: false }));
 
 connectDB();
 
-// Serve Frontend
 if (process.env.NODE_ENV === "production") {
-  // Set build folder as static
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static("frontend/build")); // set static folder
 
-  // FIX: below code fixes app crashing on refresh in deployment
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (_, res) => {
