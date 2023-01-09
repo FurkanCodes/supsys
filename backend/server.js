59f4af21d69675daa0bbd31784dbe2984e4cfd49
Furkan <turkogluvelifurkan@gmail.com>
2023-01-10 00:07:53 +0300

    CPS

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { expressCspHeader, INLINE, NONE, SELF } = require("express-csp-header");
 
 const express = require("express");
 require("dotenv").config();
@@ -18,6 +19,15 @@ connectDB();
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 app.use(errorHandler);
+
+app.use(
+  expressCspHeader({
+    directives: {
+      "default-src": [SELF],
+      "img-src": [SELF],
+    },
+  })
+);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build")); // set static folder
